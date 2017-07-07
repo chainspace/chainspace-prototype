65fee2722c30e5e470fa422b41d452e2a5e40dbe
Mustafa Al-Bassam <mus@musalbas.com>
2017-07-07 22:53:05 +0100

    split tests

--- chainspacecontract/chainspacecontract/test/examples.py
+++ chainspacecontract/chainspacecontract/test/examples.py
@@ -4,16 +4,12 @@ import unittest
 
 import requests
 
-from chainspacecontract.contract import ChainspaceContract
 from chainspacecontract.examples.increment import contract as increment_contract
 from chainspacecontract.examples.increment_with_custom_checker import contract as increment_with_custom_checker_contract
 from chainspacecontract.examples.bank_unauthenticated import contract as bank_unauthenticated_contract
-from chainspacecontract.examples.bank_authenticated import contract as bank_authenticated_contract
-from chainspacecontract.examples import bank_authenticated
 
 
-class TestIncrement(unittest.TestCase):
-    """
+class TestExamples(unittest.TestCase):
     def test_increment_checker_service(self):
         checker_service_process = Process(target=increment_contract.run_checker_service)
         checker_service_process.start()
@@ -97,46 +93,6 @@ class TestIncrement(unittest.TestCase):
 
         checker_service_process.terminate()
         checker_service_process.join()
-    """
-
-
-
-    #############################################################################################
-    # test an authenticated bank transfer
-    #############################################################################################
-    def test_bank_authenticated_checker_service(self):
-        checker_service_process = Process(target=bank_authenticated_contract.run_checker_service)
-        checker_service_process.start()
-        time.sleep(0.1)
-
-        # NOTE: export public key
-        """
-        G = EcGroup()
-        g = G.generator()
-        priv = G.order().random()
-        pub = priv * g
-        byte_string = pub.export()
-        print hexlify(byte_string)
-        print EcPt.from_binary(byte_string, G) == pub
-        """
-
-        response = requests.post('http://127.0.0.1:5000/auth_transfer', 
-            json=bank_authenticated.auth_transfer(
-                [{'name': 'alice', 'balance': 10}, {'name': 'bob', 'balance': 10}],
-                None,
-                {'amount': 3},
-                '83C72CF7E1BA9F120C5A45135A0FE3DA59D7771BB9C670B63134A8B0'
-            )
-        )
-        response_json = response.json()
-        self.assertTrue(response_json['success'])
-
-        checker_service_process.terminate()
-        checker_service_process.join()
-
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
