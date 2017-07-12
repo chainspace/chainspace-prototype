63b2fe5449442f4f2c83f3155460c1a2657da345
Mustafa Al-Bassam <mus@musalbas.com>
2017-07-12 19:28:59 +0100

    Checker interface

---
+++ chainspacecore/src/main/java/uk/ac/ucl/cs/sec/chainspace/Checker.java
@@ -0,0 +1,18 @@
+package uk.ac.ucl.cs.sec.chainspace;
+
+/**
+ * An interface representing a checker.
+ */
+public abstract class Checker {
+    abstract void startChecker();
+    abstract void stopChecker();
+    abstract Boolean check(TransactionForChecker transactionForChecker);
+
+    Boolean checkSingle(TransactionForChecker transactionForChecker) {
+        this.startChecker();
+        Boolean result = this.check(transactionForChecker);
+        this.stopChecker();
+
+        return result;
+    }
+}
