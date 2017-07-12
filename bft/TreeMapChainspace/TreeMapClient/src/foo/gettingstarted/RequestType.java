d3c06d51b08ef0acfe37624d049a5361f49d4621
Sheharbano <sheharbano.k@gmail.com>
2017-07-13 00:41:32 +0100

    Implemented prepare_t (shard0 only)

--- bft/TreeMapChainspace/TreeMapClient/src/foo/gettingstarted/RequestType.java
+++ bft/TreeMapChainspace/TreeMapClient/src/foo/gettingstarted/RequestType.java
@@ -5,8 +5,9 @@ public class RequestType {
     public static final int GET = 2;
     public static final int REMOVE = 3;
     public static final int SIZE = 4;
-    public static final int TRANSACTION = 5;
-    public static final String PREPARE_T = "prepare_t";
-    public static final String ACCEPT_T_COMMIT = "accept_t_commit";
-    public static final String ACCEPT_T_ABORT = "accept_t_abort";
+    public static final int TRANSACTION_VALIDITY = 5;
+    public static final int TRANSACTION_COMMIT = 6;
+    public static final int PREPARE_T = 7;
+    public static final int ACCEPT_T_ABORT = 8;
+    public static final int ACCEPT_T_COMMIT = 9;
 }
