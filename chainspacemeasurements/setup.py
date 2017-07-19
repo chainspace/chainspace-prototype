7544ecec3b9cb346eb8920b234c44358735077ee
Mustafa Al-Bassam <mus@musalbas.com>
2017-07-19 16:27:06 +0100

    measurements base

---
+++ chainspacemeasurements/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name = 'chainspacemeasurements',
+    version = '0.1',
+    packages = ['chainspacemeasurements'],
+    install_requires = [
+        'fabric',
+    ],
+)
