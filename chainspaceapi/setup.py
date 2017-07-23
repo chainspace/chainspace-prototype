fd1544659bf621a57f14d65b28ceb7b071a4ace9
Mustafa Al-Bassam <mus@musalbas.com>
2017-07-23 01:47:20 +0100

    basic client api

---
+++ chainspaceapi/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name = 'chainspaceapi',
+    version = '0.1',
+    packages = ['chainspaceapi'],
+    install_requires = [
+        'requests',
+    ],
+)
