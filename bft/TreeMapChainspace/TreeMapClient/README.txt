2cb2cd61894efe31fd438ee8908415df3c47ea59
Mustafa Al-Bassam <mus@musalbas.com>
2017-07-27 19:31:16 +0100

    remove bft folder

--- bft/TreeMapChainspace/TreeMapClient/README.txt
+++
@@ -1,5 +0,0 @@
-To run:
- java -classpath bin/BFT-SMaRt.jar:lib/*:out/production/TreeMapClient/ foo/gettingstarted/client/ConsoleClient ChainSpaceConfig/config.txt
-
-Configuration files here:
-ChainSpaceConfig
--- bft/TreeMapChainspace/TreeMapClient/src/foo/gettingstarted/client/Extractor.java
+++
@@ -1,7 +0,0 @@
-package foo.gettingstarted.client;
-
-import bftsmart.tom.core.messages.TOMMessage;
-
-public interface Extractor {
-    TOMMessage extractResponse(TOMMessage[] replies, int sameContent, int lastReceived);
-}
