178e0b739042c07cda0a32b369a883792377e5d9
Mustafa Al-Bassam <mus@musalbas.com>
2017-07-27 19:07:22 +0100

    remove

--- chainspacefinal/TreeMapChainspace/TreeMapClient/src/main/java/foo/gettingstarted/Config.java
+++
@@ -1,9 +0,0 @@
-package foo.gettingstarted;
-
-/**
- * Created by sheharbano on 26/07/2017.
- */
-public class Config {
-    public static final String shardConfigFile = "shardConfigFile";
-    public static final String thisClient = "thisClient";
-}
