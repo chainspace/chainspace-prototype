63ea1994aa83a5daa34c596e15b4f2bc8df2c6f5
Sheharbano <sheharbano.k@gmail.com>
2017-07-28 16:12:55 +0100

    Remove currentView before running the client

--- chainspacecore/runclient.sh
+++ chainspacecore/runclient.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
+rm config/currentView
 java -cp lib/BFT-SMaRt.jar:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.ConsoleClient ChainSpaceClientConfig/config.txt
