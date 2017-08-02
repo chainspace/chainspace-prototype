1a6949d75d2ca4f12c40220e94697d4c1153e0c8
Mustafa Al-Bassam <mus@musalbas.com>
2017-08-02 02:08:28 +0100

    dumper

--- chainspacemeasurements/chainspacemeasurements/dumper.py
+++ chainspacemeasurements/chainspacemeasurements/dumper.py
@@ -8,7 +8,7 @@ client = ChainspaceClient()
 
 
 def dump(transaction):
-    client.process_transaction(transaction)
+    client.dump_transaction(transaction)
 
 
 def dump_many(transactions):
@@ -16,7 +16,40 @@ def dump_many(transactions):
         dump(transaction)
 
 
-def dump_a(n):
+def process(transaction):
+    client.process_transaction(transaction)
+
+
+def process_many(transactions):
+    for transaction in transactions:
+        process(transaction)
+
+
+def simulation_a1(n):
+    init_tx = simulator.init()
+    process(init_tx)
+
+    create_tx = simulator.create((init_tx['transaction']['outputs'][0],), None, (str(n),))
+    process(create_tx)
+
+    outputs = create_tx['transaction']['outputs']
+    transactions = [simulator.consume((output,)) for output in outputs]
+    process_many(transactions)
+
+
+def simulation_a2(n):
+    init_tx = simulator.init()
+    process(init_tx)
+
+    create_tx = simulator.create((init_tx['transaction']['outputs'][0],), None, (str(n),))
+    process(create_tx)
+
+    outputs = create_tx['transaction']['outputs']
+    transactions = [simulator.consume((output,)) for output in outputs]
+    dump_many(transactions)
+
+
+def simulation_a3(n):
     init_tx = simulator.init()
     dump(init_tx)
 
