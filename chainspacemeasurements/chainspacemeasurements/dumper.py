3e2e3a31f55d93a0a959bab67a07acd10a7a929d
Mustafa Al-Bassam <mus@musalbas.com>
2017-08-03 20:41:27 +0100

    add new simulation

--- chainspacemeasurements/chainspacemeasurements/dumper.py
+++ chainspacemeasurements/chainspacemeasurements/dumper.py
@@ -59,3 +59,22 @@ def simulation_a3(n):
     outputs = create_tx['transaction']['outputs']
     transactions = [simulator.consume((output,)) for output in outputs]
     dump_many(transactions)
+
+
+def simulation_b2(n, inputs_per_tx):
+    init_tx = simulator.init()
+    process(init_tx)
+
+    create_tx = simulator.create((init_tx['transaction']['outputs'][0],), None, (str(n),))
+    process(create_tx)
+
+    outputs = create_tx['transaction']['outputs']
+
+    transactions = []
+    for i in range(0, len(outputs), inputs_per_tx):
+        objects = []
+        for j in range(inputs_per_tx):
+            objects.append(outputs[i+j])
+        transactions.append(simulator.consume(objects))
+
+    dump_many(transactions)
