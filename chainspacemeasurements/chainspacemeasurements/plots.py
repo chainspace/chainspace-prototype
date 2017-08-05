3dc5af59b5ec7136634af304b08fabffc5bd68ac
Mustafa Al-Bassam <mus@musalbas.com>
2017-08-05 22:39:43 +0100

    graph plotting

--- chainspacemeasurements/chainspacemeasurements/plots.py
+++ chainspacemeasurements/chainspacemeasurements/plots.py
@@ -1,8 +1,27 @@
+import json
+import sys
+
 from matplotlib import pyplot
 
+from chainspacemeasurements.results import parse_shard_scaling
 
-pyplot.xlabel('Number of shards')
-pyplot.ylabel('Transactions / sec')
-pyplot.grid(True)
-pyplot.savefig('test.pdf')
-pyplot.close()
+
+def plot_shard_scaling(results, outfile):
+    parsed_results = parse_shard_scaling(results)
+    pyplot.xlabel('Number of shards')
+    pyplot.ylabel('Transactions / sec')
+    pyplot.grid(True)
+
+    pyplot.plot(
+        range(2, len(parsed_results)+2),
+        [i[0] for i in parsed_results]
+    )
+
+    pyplot.savefig(outfile)
+    pyplot.close()
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'shardscaling':
+        results = json.loads(open(sys.argv[2]).read())
+        plot_shard_scaling(results, sys.argv[3])
