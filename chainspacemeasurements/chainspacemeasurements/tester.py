bc9568eb66f8ba7e89d2f19c707f6e6b4652bf47
Mustafa Al-Bassam <mus@musalbas.com>
2017-08-05 13:46:57 +0100

    fix

--- chainspacemeasurements/chainspacemeasurements/tester.py
+++ chainspacemeasurements/chainspacemeasurements/tester.py
@@ -57,7 +57,7 @@ class Tester(object):
 
                     tps_set = self.network.get_tps_set()
                     tps_sets.append(tps_set)
-                    print "Result for {0} shards (run {1}): {}".format(num_shards, i, tps_set)
+                    print "Result for {0} shards (run {1}): {2}".format(num_shards, i, tps_set)
                 except Exception:
                     traceback.print_exc()
                 finally:
