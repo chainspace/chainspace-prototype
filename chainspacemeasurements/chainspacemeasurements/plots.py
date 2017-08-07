17b70b89d17836a67fe4515f81c7e63874f293d3
Mustafa Al-Bassam <mus@musalbas.com>
2017-08-07 18:05:35 +0100

    inputscaling

--- chainspacemeasurements/chainspacemeasurements/plots.py
+++ chainspacemeasurements/chainspacemeasurements/plots.py
@@ -3,7 +3,7 @@ import sys
 
 from matplotlib import pyplot
 
-from chainspacemeasurements.results import parse_shard_scaling
+from chainspacemeasurements.results import parse_shard_scaling, parse_input_scaling_2
 
 
 def plot_shard_scaling(results, outfile):
@@ -24,7 +24,27 @@ def plot_shard_scaling(results, outfile):
     pyplot.close()
 
 
+def plot_input_scaling_2(results, outfile):
+    parsed_results = parse_input_scaling_2(results)
+    pyplot.xlabel('Number of inputs per transaction')
+    pyplot.ylabel('Average transactions / second')
+    pyplot.grid(True)
+
+    pyplot.errorbar(
+        range(2, len(parsed_results)+2),
+        [i[0] for i in parsed_results],
+        [i[1] for i in parsed_results],
+        marker='o',
+    )
+
+    pyplot.savefig(outfile)
+    pyplot.close()
+
+
 if __name__ == '__main__':
     if sys.argv[1] == 'shardscaling':
         results = json.loads(open(sys.argv[2]).read())
         plot_shard_scaling(results, sys.argv[3])
+    elif sys.argv[1] == 'inputscaling2':
+        results = json.loads(open(sys.argv[2]).read())
+        plot_input_scaling_2(results, sys.argv[3])
--- chainspacemeasurements/chainspacemeasurements/results.py
+++ chainspacemeasurements/chainspacemeasurements/results.py
@@ -19,3 +19,18 @@ def parse_shard_scaling(results):
         final_result.append((mean, sd))
 
     return final_result
+
+
+def parse_input_scaling_2(results):
+    final_result = []
+    for shard in results:
+        sum_set = []
+        for tps_set in shard:
+            sum_set.append(sum(tps_set))
+
+        mean = numpy.mean(sum_set)
+        sd = numpy.std(sum_set)
+
+        final_result.append((mean, sd))
+
+    return final_result
