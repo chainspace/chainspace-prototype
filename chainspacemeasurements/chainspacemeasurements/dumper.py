0cb53bd0d3f929f4d2da743207e9993606a3dabc
Mustafa Al-Bassam <mus@musalbas.com>
2017-08-08 22:39:16 +0100

    fix

--- chainspacemeasurements/chainspacemeasurements/dumper.py
+++ chainspacemeasurements/chainspacemeasurements/dumper.py
@@ -121,22 +121,19 @@ def simulation_batched(n, inputs_per_tx, batch_size=100, batch_sleep=2, nonce=Tr
         for output in outputs:
             outputs_map[map_object_to_shard(num_shards, output)].append(output)
 
-    transactions = []
-    try:
-        for i in range(0, len(outputs), inputs_per_tx):
-            if shards_per_tx is not None:
-                rand = random.randint(0, num_shards-shards_per_tx)
-            objects = []
-            for j in range(inputs_per_tx):
-                if shards_per_tx is None:
-                    objects.append(outputs[i+j])
-                else:
-                    shard_id = j%shards_per_tx
-                    shard_id = shard_id + rand
-                    objects.append(outputs_map[shard_id].pop())
-            transactions.append(simulator.consume(objects))
-    except IndexError:
-        pass # ran out of objects to pop
+    transactions =
+    for i in range(0, len(outputs), inputs_per_tx):
+        if shards_per_tx is not None:
+            rand = random.randint(0, num_shards-shards_per_tx)
+        objects = []
+        for j in range(inputs_per_tx):
+            if shards_per_tx is None:
+                objects.append(outputs[i+j])
+            else:
+                shard_id = j%shards_per_tx
+                shard_id = shard_id + rand
+                objects.append(outputs_map[shard_id].pop())
+        transactions.append(simulator.consume(objects))
 
     for i in range(0, len(transactions), batch_size):
         dump_many(transactions[i:i+batch_size])
