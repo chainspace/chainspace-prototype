16f2f9f4315450422e15ef7fc130e4f4f60795a5
Mustafa Al-Bassam <mus@musalbas.com>
2017-08-08 04:23:20 +0100

    forscript sysconfig

--- chainspacemeasurements/chainspacemeasurements/plots.py
+++ chainspacemeasurements/chainspacemeasurements/plots.py
@@ -41,6 +41,23 @@ def plot_input_scaling_2(results, outfile):
     pyplot.close()
 
 
+def plot_node_scaling(results, outfile):
+    parsed_results = parse_input_scaling_2(results)
+    pyplot.xlabel('Number of replicas per shard')
+    pyplot.ylabel('Average transactions / second')
+    pyplot.grid(True)
+
+    pyplot.errorbar(
+        range(4, len(parsed_results)+4),
+        [i[0] for i in parsed_results],
+        [i[1] for i in parsed_results],
+        marker='o',
+    )
+
+    pyplot.savefig(outfile)
+    pyplot.close()
+
+
 if __name__ == '__main__':
     if sys.argv[1] == 'shardscaling':
         results = json.loads(open(sys.argv[2]).read())
@@ -48,3 +65,6 @@ if __name__ == '__main__':
     elif sys.argv[1] == 'inputscaling2':
         results = json.loads(open(sys.argv[2]).read())
         plot_input_scaling_2(results, sys.argv[3])
+    elif sys.argv[1] == 'nodescaling':
+        results = json.loads(open(sys.argv[2]).read())
+        plot_node_scaling(results, sys.argv[3])
