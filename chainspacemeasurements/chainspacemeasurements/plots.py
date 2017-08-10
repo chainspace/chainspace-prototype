b934f948631fc0d64abdbe3408ae05b31d8b4b1d
Mustafa Al-Bassam <mus@musalbas.com>
2017-08-10 11:03:56 +0100

    add steps to nodescale plotting

--- chainspacemeasurements/chainspacemeasurements/plots.py
+++ chainspacemeasurements/chainspacemeasurements/plots.py
@@ -41,14 +41,14 @@ def plot_input_scaling(results, outfile):
     pyplot.close()
 
 
-def plot_node_scaling(results, outfile):
+def plot_node_scaling(results, outfile, step):
     parsed_results = parse_shard_results(results)
     pyplot.xlabel('Number of replicas per shard')
     pyplot.ylabel('Average transactions / second')
     pyplot.grid(True)
 
     pyplot.errorbar(
-        range(4, len(parsed_results)+4),
+        range(4, 4+len(parsed_results)*step, step),
         [i[0] for i in parsed_results],
         [i[1] for i in parsed_results],
         marker='o',
@@ -64,7 +64,7 @@ if __name__ == '__main__':
         plot_shard_scaling(results, sys.argv[3])
     elif sys.argv[1] == 'inputscaling':
         results = json.loads(open(sys.argv[2]).read())
-        plot_input_scaling_2(results, sys.argv[3])
+        plot_input_scaling(results, sys.argv[3])
     elif sys.argv[1] == 'nodescaling':
         results = json.loads(open(sys.argv[2]).read())
-        plot_node_scaling(results, sys.argv[3])
+        plot_node_scaling(results, sys.argv[3], int(sys.argv[4]))
