f200936d0c0ff65705034fe2febd9adc5e9e53d0
Mustafa Al-Bassam <mus@musalbas.com>
2017-08-11 02:27:47 +0100

    fix labels

--- chainspacemeasurements/chainspacemeasurements/plots.py
+++ chainspacemeasurements/chainspacemeasurements/plots.py
@@ -3,7 +3,7 @@ import sys
 
 from matplotlib import pyplot
 
-from chainspacemeasurements.results import parse_shard_results
+from chainspacemeasurements.results import parse_shard_results, parse_client_latency_results
 
 
 def plot_shard_scaling(results, outfile):
@@ -58,6 +58,25 @@ def plot_node_scaling(results, outfile, step):
     pyplot.close()
 
 
+def plot_client_latency(results, outfile, start_tps, step):
+    parsed_results = parse_client_latency_results(results)
+    pyplot.xlabel('Client-perceived latency (ms)')
+    pyplot.ylabel('Fraction of transactions')
+    pyplot.grid(True)
+
+    for i, tps in enumerate(parsed_results):
+        tps = [x*1000 for x in tps]
+        pyplot.plot(
+            tps,
+            [j/float(len(tps)) for j in range(len(tps))],
+            label=str(start_tps+i*step) + ' t/s',
+        )
+
+    pyplot.legend()
+    pyplot.savefig(outfile)
+    pyplot.close()
+
+
 if __name__ == '__main__':
     if sys.argv[1] == 'shardscaling':
         results = json.loads(open(sys.argv[2]).read())
@@ -68,3 +87,6 @@ if __name__ == '__main__':
     elif sys.argv[1] == 'nodescaling':
         results = json.loads(open(sys.argv[2]).read())
         plot_node_scaling(results, sys.argv[3], int(sys.argv[4]))
+    elif sys.argv[1] == 'clientlatency':
+        results = json.loads(open(sys.argv[2]).read())
+        plot_client_latency(results, sys.argv[3], int(sys.argv[4]), int(sys.argv[5]))
