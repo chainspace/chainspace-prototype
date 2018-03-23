3e6d535ed6325b2441abcb420af44419ca497f43
Jim Barritt <jimb@thoughtworks.com>
2018-03-23 22:36:46 +0000

    Replacing BFTSmart Jar with DECODE fork from muslalbas

--- chainspacecore/runclientservice.sh
+++ chainspacecore/runclientservice.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+
+
+BFT_SMART_LIB=lib/bft-smart-1.1-UCL.jar
+
+
 rm config/currentView
-java -cp lib/BFT-SMaRt.jar:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.Client ChainSpaceClientConfig/config.txt
+java -cp ${BFT_SMART_LIB}:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.Client ChainSpaceClientConfig/config.txt
--- contrib/core-tools/easystart.mac.sh
+++ contrib/core-tools/easystart.mac.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 rm -rf chainspacecore-*
 
+BFT_SMART_LIB=lib/bft-smart-1.1-UCL.jar
+
 cp -r chainspacecore chainspacecore-0-0
 cd chainspacecore-0-0
 printf "shardConfigFile ChainSpaceConfig/shardConfig.txt\nthisShard 0\nthisReplica 0" > ChainSpaceConfig/config.txt
 cp config/hosts.config.0 config/hosts.config
-screen -dmSL s0n0 java -Dchecker.start.port=13010 -cp lib/BFT-SMaRt.jar:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
+screen -dmSL s0n0 java -Dchecker.start.port=13010 -cp ${BFT_SMART_LIB}:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
 cd ../
 sleep 1
 
@@ -13,7 +15,7 @@ cp -r chainspacecore chainspacecore-0-1
 cd chainspacecore-0-1
 printf "shardConfigFile ChainSpaceConfig/shardConfig.txt\nthisShard 0\nthisReplica 1" > ChainSpaceConfig/config.txt
 cp config/hosts.config.0 config/hosts.config
-screen -dmSL s0n1 java -Dchecker.start.port=14010 -cp lib/BFT-SMaRt.jar:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
+screen -dmSL s0n1 java -Dchecker.start.port=14010 -cp ${BFT_SMART_LIB}:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
 cd ../
 sleep 1
 
@@ -21,7 +23,7 @@ cp -r chainspacecore chainspacecore-0-2
 cd chainspacecore-0-2
 printf "shardConfigFile ChainSpaceConfig/shardConfig.txt\nthisShard 0\nthisReplica 2" > ChainSpaceConfig/config.txt
 cp config/hosts.config.0 config/hosts.config
-screen -dmSL s0n2 java -Dchecker.start.port=15010 -cp lib/BFT-SMaRt.jar:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
+screen -dmSL s0n2 java -Dchecker.start.port=15010 -cp ${BFT_SMART_LIB}:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
 cd ../
 sleep 1
 
@@ -29,7 +31,7 @@ cp -r chainspacecore chainspacecore-0-3
 cd chainspacecore-0-3
 printf "shardConfigFile ChainSpaceConfig/shardConfig.txt\nthisShard 0\nthisReplica 3" > ChainSpaceConfig/config.txt
 cp config/hosts.config.0 config/hosts.config
-screen -dmSL s0n3 java -Dchecker.start.port=16010 -cp lib/BFT-SMaRt.jar:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
+screen -dmSL s0n3 java -Dchecker.start.port=16010 -cp ${BFT_SMART_LIB}:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
 cd ../
 sleep 1
 
@@ -37,7 +39,7 @@ cp -r chainspacecore chainspacecore-1-0
 cd chainspacecore-1-0
 printf "shardConfigFile ChainSpaceConfig/shardConfig.txt\nthisShard 1\nthisReplica 0" > ChainSpaceConfig/config.txt
 cp config/hosts.config.1 config/hosts.config
-screen -dmSL s1n0 java -Dchecker.start.port=17010 -cp lib/BFT-SMaRt.jar:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
+screen -dmSL s1n0 java -Dchecker.start.port=17010 -cp ${BFT_SMART_LIB}:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
 cd ../
 sleep 1
 
@@ -45,7 +47,7 @@ cp -r chainspacecore chainspacecore-1-1
 cd chainspacecore-1-1
 printf "shardConfigFile ChainSpaceConfig/shardConfig.txt\nthisShard 1\nthisReplica 1" > ChainSpaceConfig/config.txt
 cp config/hosts.config.1 config/hosts.config
-screen -dmSL s1n1 java -Dchecker.start.port=18010 -cp lib/BFT-SMaRt.jar:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
+screen -dmSL s1n1 java -Dchecker.start.port=18010 -cp ${BFT_SMART_LIB}:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
 cd ../
 sleep 1
 
@@ -53,7 +55,7 @@ cp -r chainspacecore chainspacecore-1-2
 cd chainspacecore-1-2
 printf "shardConfigFile ChainSpaceConfig/shardConfig.txt\nthisShard 1\nthisReplica 2" > ChainSpaceConfig/config.txt
 cp config/hosts.config.1 config/hosts.config
-screen -dmSL s1n2 java -Dchecker.start.port=19010 -cp lib/BFT-SMaRt.jar:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
+screen -dmSL s1n2 java -Dchecker.start.port=19010 -cp ${BFT_SMART_LIB}:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
 cd ../
 sleep 1
 
@@ -61,5 +63,5 @@ cp -r chainspacecore chainspacecore-1-3
 cd chainspacecore-1-3
 printf "shardConfigFile ChainSpaceConfig/shardConfig.txt\nthisShard 1\nthisReplica 3" > ChainSpaceConfig/config.txt
 cp config/hosts.config.1 config/hosts.config
-screen -dmSL s1n3 java -Dchecker.start.port=20010 -cp lib/BFT-SMaRt.jar:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
+screen -dmSL s1n3 java -Dchecker.start.port=20010 -cp ${BFT_SMART_LIB}:target/chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.ucl.cs.sec.chainspace.bft.TreeMapServer ChainSpaceConfig/config.txt
 cd ../
