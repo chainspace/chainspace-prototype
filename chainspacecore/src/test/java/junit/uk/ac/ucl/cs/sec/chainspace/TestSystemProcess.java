19a0a6ea63db1a6dc96bd93026f236e3df15fefe
Jim Barritt <jimb@thoughtworks.com>
2018-04-03 07:32:45 +0100

    Fixed typo

--- chainspacecore/src/test/java/junit/uk/ac/ucl/cs/sec/chainspace/TestSystemProcess.java
+++ chainspacecore/src/test/java/junit/uk/ac/ucl/cs/sec/chainspace/TestSystemProcess.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertThat;
 public class TestSystemProcess {
 
     @Test
-    public void writes_proccess_id_to_file() {
+    public void writes_process_id_to_file() {
         File idFile = new File("test.process.id");
 
         SystemProcess.writeProcessIdToFile(idFile.getAbsolutePath());
