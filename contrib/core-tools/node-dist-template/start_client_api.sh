2d4a291577839a391ef69645d6578478002d8e35
Jim Barritt <jimb@thoughtworks.com>
2018-04-03 08:13:49 +0100

    Fixed databse config for client-api

--- contrib/core-tools/node-dist-template/start_client_api.sh
+++ contrib/core-tools/node-dist-template/start_client_api.sh
@@ -5,7 +5,7 @@ echo -e "\nStarting Chainspace Client API...\n"
 BFT_SMART_LIB=lib/bft-smart-1.2-UCL.jar
 CHAINSPACE_JAR=chainspace-1.0-SNAPSHOT-jar-with-dependencies.jar
 CLIENT_API_PORT=5000
-CLIENT_API_DB=../node-0-0/database
+CLIENT_API_DB=../node_0_0/database
 MAIN_CLASS=uk.ac.ucl.cs.sec.chainspace.Client
 CONFIG_FILE=config/client-api/config.txt
 
