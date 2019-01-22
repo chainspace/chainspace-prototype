e5e8f3829aaaa2aad77b2a4e14bd1a174b1334c4
Mustafa Al-Bassam <mus@musalbas.com>
2019-01-22 16:50:05 +0000

    Remove debug prints

--- chainspaceapi/chainspaceapi/api.py
+++ chainspaceapi/chainspaceapi/api.py
@@ -12,12 +12,7 @@ class ChainspaceClient(object):
 
     def process_transaction(self, transaction):
         endpoint = self.url + '/api/1.0/transaction/process'
-        print "POST " + endpoint + " HTTP/1.1"
-        print "" + json.dumps(transaction)
         r = requests.post(endpoint, json=transaction)
-
-        print "HTTP/1.1 " + str(r.status_code) + " " + r.reason
-        print r.json()
         return r
 
     def dump_transaction(self, transaction):
